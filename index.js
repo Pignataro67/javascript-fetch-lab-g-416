07677d55b983542c5c4167bd1b7596257398ff0f
Pat  Pignataro <pat6284@msn.com>
2019-05-10 13:25:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,95 @@
+// const userName = 'alecgalba'
+// const apiUrl = 'https://api.github.com'
+// const repo = 'alecgalba/javascript-fetch-lab'
+
+// function getIssues() {
+//   fetch(`${apiUrl}/repos/${repo}/issues`)
+//     .then(res => showIssues(res.json()))
+// }
+
+// function showIssues(json) {
+//   const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+//   document.getElementById('issues').innerHTML = template(json)
+// }
+
+// function createIssue() {
+//   const issueTitle = document.getElementById('title').value
+//   const issueBody = document.getElementById('body').value
+//   const postData = { title: issueTitle, body: issueBody }
+
+//   fetch(`${apiUrl}/repos/${repo}/issues`, {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `token ${getToken()}`
+//     },
+//     body: JSON.stringify(postData)
+//   }).then(res => getIssues())
+// }
+
+// function showResults(json) {
+//   const template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
+//   document.getElementById('results').innerHTML = template(json)
+// }
+
+// function forkRepo() {
+//   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+//   //use fetch to fork it!
+// }
+
+// function getToken() {
+//   fetch(`${apiUrl}/repos/${repo}/forks`, {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `token ${getToken()}`
+//     }
+//   }).then(res => showResults(res.json()))
+//   return ''
+// }
+
+const userName = 'alecgalba'
+const apiUrl = 'https://api.github.com'
+const repo = 'alecgalba/javascript-fetch-lab'
+
 function getIssues() {
+  fetch(`${apiUrl}/repos/${repo}/issues`)
+    .then(res => showIssues(res.json()))
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+
+  fetch(`${apiUrl}/repos/${repo}/issues`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(res => getIssues())
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`${apiUrl}/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => showResults(res.json()))
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
-}
